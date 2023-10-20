35c001deb76ddc64892af91dc9629972814a32c9
samuelochaba <nerdysam5826@gmail.com>
2023-10-20 16:38:18 +0100

    add cli tool

--- index.js
+++ index.js
@@ -2,13 +2,30 @@
 
 const chokidar = require("chokidar");
 const debounce = require("lodash.debounce");
+const program = require("caporal");
+const fs = require("fs");
 
-const start = debounce(() => {
-  console.log("starting users program");
-}, 100);
+program
+  .version("0.0.1")
+  .argument("[filename", "Name of a file to execute")
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
 
-chokidar
-  .watch(".")
-  .on("add", start)
-  .on("change", () => console.log("file changed"))
-  .on("unlink", () => console.log("file unlinked"));
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
+
+    const start = debounce(() => {
+      console.log("starting users program");
+    }, 100);
+
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
+  });
+
+program.parse(process.argv);
